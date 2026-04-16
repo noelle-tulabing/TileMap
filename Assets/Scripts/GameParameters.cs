e0915a6d3f82c147dff3e8be62ecb5718b5dc5a2
Noelle Tulabing <noelle.tulabing@gmail.com>
2026-04-16 15:45:58 -0700

    Tile Map day 4: Enemy

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -5,6 +5,15 @@ public static class GameParameters
 {
     public static float PlayerSpeed = 5f;
     public static float PlayerAttackCooldown = 1f;
+
+    public static int EnemyDamage = 1;
+    public static float EnemySpeed = 3f;
+    public static int EnemyAttackRange = 2;
+    public static int EnemyAttackCooldown = 2;
+    public static int EnemyDetectRange = 5;
+    public static float EnemyMinSpawnDelay = 1f;
+    public static float EnemyMaxSpawnDelay = 3f;
+    public static int EnemyMaxSpawnAttempts = 100; // how many enemies to defeat to end/win game
     
     public static Key MoveLeft  = Key.A;
     public static Key MoveRight = Key.D;
@@ -16,4 +25,8 @@ public static class GameParameters
     public static Key PickupKey = Key.F;
 
     public static int MaxHealth = 10;
+
+    
+    
+
 }
